78f0981fef52e540ab4f2e493d9fc162d05e3994
Wulf48 <torwuld@mail.ru>
2019-09-22 00:12:02 +0700

    Delete functions in file read

--- src/read.cpp
+++ src/read.cpp
@@ -16,23 +16,3 @@ bool check_read() {
 
   return true;
 }
-
-void print_file_rus() {
-  string Tempstr;
-  ifstream Translate("translate");
-
-  while (getline(Translate, Tempstr))
-    cout << Tempstr << endl;
-
-  Translate.close();
-}
-
-void print_file_eng() {
-  string Tempstr;
-  ifstream Wordbook("words");
-
-  while (getline(Wordbook, Tempstr))
-    cout << Tempstr << endl;
-
-  Wordbook.close();
-}
