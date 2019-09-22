4f3e7a137f102c628e8be4519a33a7cb8be49ec8
Wulf48 <torwuld@mail.ru>
2019-09-23 01:32:06 +0700

    Add function transform

--- src/comparison.cpp
+++ src/comparison.cpp
@@ -1,8 +1,13 @@
+#include <algorithm>
 #include <cstring>
 #include <fstream>
 #include <iostream>
 
 using namespace std;
+
+void input_str(string& Input) {
+  cin >> Input;
+}
 
 void input_words_eng(ifstream& Translate, ifstream& Words) {
   string Input, Tempstr, Temps;
@@ -10,8 +15,8 @@ void input_words_eng(ifstream& Translate, ifstream& Words) {
 
   while (getline(Translate, Tempstr) && getline(Words, Temps)) {
     cout << Tempstr << "-";
-    cin >> Input;
-
+    input_str(Input);
+    transform(Input.begin(), Input.end(), Input.begin(), ::tolower);
     Result = Temps.compare(Input);
     if (Result == 0) {
       cout << "Верно\n";
@@ -25,10 +30,10 @@ void input_words_eng(ifstream& Translate, ifstream& Words) {
 void input_translate(ifstream& Translate, ifstream& Words) {
   string Tempstr, Input, Temps;
   int Result;
-
+  input_str(Input);
   while (getline(Translate, Tempstr) && getline(Words, Temps)) {
     cout << Temps << "-";
-    cin >> Input;
+    transform(Input.begin(), Input.end(), Input.begin(), ::tolower);
     Result = Tempstr.compare(Input);
     if (Result == 0) {
       cout << "Верно\n";
