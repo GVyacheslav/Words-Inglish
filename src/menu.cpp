869b832939b8bb448f8e3cc4d83ba245a360a5d2
Wulf48 <torwuld@mail.ru>
2019-09-23 11:55:45 +0700

    Add function check_input_data

--- src/menu.cpp
+++ src/menu.cpp
@@ -13,23 +13,29 @@ void text_menu() {
   cout << "Выберите действие\n";
   cout << "1) Упражнение для запоминания на английском языке\n";
   cout << "2) Упражнение для запоминания на русском языке\n";
+  cout << "3) Выход из программы\n";
+}
+
+void check_input_data(int& step, int& temp) {
+  do {
+    temp = scanf("%d", &step);
+    while (getchar() != '\n')
+      ;
+    if (temp != 1 || temp != 2 || temp != 3) {
+      cout << "Некорректный ввод данных!\n";
+    }
+  } while (temp != 1 && temp != 2 && temp != 3);
 }
 
 void menu(ifstream& Translate, ifstream& Words) {
-  int step = 0, temp = 0;
+  int step = 0, temp = 0, Result;
+  string Input, Tempstr, Temps;
 
   if (check_read(Translate, Words) == true) {
     while (true) {
       text_menu();
 
-      do {
-        temp = scanf("%d", &step);
-        while (getchar() != '\n')
-          ;
-        if (temp != 1 || temp != 2) {
-          cout << "Некорректный ввод данных!\n";
-        }
-      } while (temp != 1 && temp != 2);
+      check_input_data(step, temp);
 
       system("clear");
       text_menu();
@@ -38,15 +44,15 @@ void menu(ifstream& Translate, ifstream& Words) {
       case 1:
         system("clear");
         cout << "Введите перевод с русского на английский язык\n" << endl;
-        input_words_eng(Translate, Words);
-        break;
-        system("clear");
 
-      case 2:
+        input_words_eng(Result, Input, Tempstr, Temps, Translate, Words);
         system("clear");
         cout << "Введите перевод с английского на русский язык\n" << endl;
-        input_translate(Translate, Words);
+        input_translate(Result, Input, Tempstr, Temps, Translate, Words);
         break;
+
+      case 3:
+        exit(1);
         system("clear");
       }
     }
